9e90059f4a8ab25d88ae7879d742726f759248c3
Garry Li <leo3124894688@gmail.com>
2025-10-27 23:33:33 +1100

    change the file structure

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/oakd_driver.launch.py']),
-        ('share/' + package_name + '/config', ['config/oakd_params.yaml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=[
         'setuptools',
